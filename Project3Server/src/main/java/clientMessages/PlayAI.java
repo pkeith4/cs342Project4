b68d9acbcbd6ed27303fd0058dc70803ed2bca3d
Philip Keith <pkeith4@github.com>
2024-04-27 22:04:13 -0500

    Created messages for ai

---
+++ Project3Server/src/main/java/clientMessages/PlayAI.java
@@ -0,0 +1,9 @@
+package clientMessages;
+
+import java.io.Serializable;
+
+public class PlayAI implements Serializable {
+    static final long serialVersionUID = 1;
+
+    public PlayAI() { /* do nothing */ }
+}
